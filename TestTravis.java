07fae62ab6fd3def482b437f06170ee4c9114a23
lucaardolino <43397837+lucaardolino@users.noreply.github.com>
2018-12-12 21:23:03 +0100

    Update TestTravis.java

--- TestTravis.java
+++ TestTravis.java
@@ -6,8 +6,8 @@ public class TestTravis
 	public static void main(String[] args)
 	{
 		int a=4;
-        int b=5;
-        int c=a+b;
-        System.out.println(c);
+        	int b=5;
+        	int c=a+b;
+        	System.out.println(c);
 	}        
 }
