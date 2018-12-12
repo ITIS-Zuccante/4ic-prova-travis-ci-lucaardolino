f4656dbebf30baee3869673f5f9218a71eb9b5b0
lucaardolino <43397837+lucaardolino@users.noreply.github.com>
2018-12-12 21:08:38 +0100

    test

---
+++ TestTravis.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class TestTravis
+{
+	public static void main(String[] args)
+	{
+		int a=4;
+        int b=5;
+        int c=a+b;
+        System.out.println(c);
+	}        
+}
